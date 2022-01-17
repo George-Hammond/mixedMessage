d576b0ce3e60b03eca12ace934aa5c0dac164b6f
George Hammond <george.hammond@amalitech.org>
2022-01-17 02:53:28 +0000

    mixedMessage initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const collectiveAdvice = {
+    diabetesTip: ['learn about the disease', 'know the disease and its impact on your body thoroughly', 'eat less salt', 'eat more fruits and vegetables'],
+    healthTips: ['check your diet', 'exercise frequently to stay in shape', 'monitor your sugar in-take', 'avoid foods containing high levels of cholestrol'],
+    doctorgivingTip: ['E.A. Quao', 'M.S. Nyan', 'A.B. Kwakye', 'D.N. Mkrumah']
+  }
+  
+ let messageArr = []; 
